b524ac2d7556cf0d63da165bccc9aed2e3b1ebed
Pablo Lizardo <pablolizardo@mbp.local>
2020-08-15 15:55:10 -0300

    unifying styles and colors, extract to _variables scss

--- src/landing/Header.js
+++ src/landing/Header.js
@@ -19,12 +19,12 @@ export default function Header() {
     setTimeout(() => { window.location.href = '/' }, 150);
   }
 
-  return <header className="flex items-center justify-between w-full  p-2 ">
+  return <nav className="flex items-center justify-between w-full  p-2 ">
     <h3 className="text-3xl font-bold items-center flex hover:text-gray-300 cursor-pointer"  onClick={handleGoHome}><i className='fa fa-mountain mr-2 text-base ' />Reconmap</h3>
     <nav className="font-semibold gap-5 flex items-center">
       {LINKS.map((link, index) => (<Link key={index} className={`text-gray-500 font-bold   py-1 text-base hover:text-white hover:border-white`} to={ link.to } target="_blank"> {link.title} </Link>))}
-      {authContext.logged ? <button onClick={handleLogOut} className='rounded bg-gray-800 py-2 px-4'>Log out</button>
-      : <Link to='login' className='rounded bg-gray-800 py-2 px-4'>Log in</Link>}
+      {authContext.logged ? <button onClick={handleLogOut} >Log out</button>
+      : <Link  to='login' ><button>Log in</button></Link>}
     </nav>
-  </header>
+  </nav>
 }
