b8b87940bba3cec49397c6feb929617228efd5b2
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-08-15 10:53:18 +0100

    Include version number in footer

--- src/landing/Footer.js
+++ src/landing/Footer.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Footer() {
   return <footer className="py-5 text-gray-500 font-medium">
-      Design by <strong className="text-white">sliz</strong>
+      Version <strong className="text-white">0.0.1</strong>
     </footer>
 }
