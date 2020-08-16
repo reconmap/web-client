4425804a27bd1acc196a16471a17bae663e9870b
Pablo Lizardo <pablolizardo@mbp.local>
2020-08-16 14:47:53 -0300

    remove warning on login and change the header in logged in status

--- src/landing/Login.js
+++ src/landing/Login.js
@@ -63,7 +63,7 @@ const Login = () => {
               Password
             </label>
             <input type="password" id="inputPassword" onChange={handlePassword} placeholder="Password" required />
-            <button onClick={handleLogin} to="dashboard" className={ (!credentials.username || !credentials.password) && 'opacity-50 hover:bg-gray-800' } disabled={!credentials.username || !credentials.password}>
+            <button onClick={handleLogin} to="dashboard" className={ (!credentials.username || !credentials.password) ? 'opacity-50 hover:bg-gray-800' : ''} disabled={!credentials.username || !credentials.password}>
               {!loading ? "Sign in" : "Espere por favor"}
             </button>
             <div className="checkbox my-3 text-gray-500">
