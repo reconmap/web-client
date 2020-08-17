a835fb7e9d4a48eac0e23929c8ff469ef94c02d3
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-08-17 18:05:10 +0100

    Re-order links based on their use

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,11 +1,11 @@
 export default [
     { title: 'Dashboard', icon: 'columns', to: '/dashboard/dashboard' },
     { title: 'Vulnerabilities', icon: 'alert-circle', to: '/dashboard/vulnerabilities' },
-    { title: 'User and permissions', icon: 'users', to: '/dashboard/users' },
-    { title: 'Integrations', icon: 'cpu', to: '/dashboard/integrations' },
     { title: 'Tasks', icon: 'check-square', to: '/dashboard/tasks' },
+    { title: 'Projects', icon: 'layers', to: '/dashboard/projects' },
     { title: 'Templates', icon: 'clipboard', to: '/dashboard/templates' },
     { title: 'Reports', icon: 'pie-chart', to: '/dashboard/reports' },
     { title: 'Audit log', icon: 'file-text', to: '/dashboard/auditlog' },
-    { title: 'Projects', icon: 'layers', to: '/dashboard/projects' }
+    { title: 'User and permissions', icon: 'users', to: '/dashboard/users' },
+    { title: 'Integrations', icon: 'cpu', to: '/dashboard/integrations' },
   ]
