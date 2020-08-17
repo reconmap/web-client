8ed2c65f30f650dfabd1a157f951f115932f4766
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-08-17 21:45:59 +0100

    Add export audit log button

--- src/components/auditlog/List.js
+++ src/components/auditlog/List.js
@@ -6,6 +6,29 @@ class AuditLogList extends Component {
         auditLog: []
     }
 
+    handleExport() {
+        fetch(`${configuration.api.baseUrl}/auditlog/export`, {
+            method: 'GET',
+            headers: {
+                Authorization: 'Bearer ' + localStorage.getItem('accessToken')
+            }
+        })
+            .then(response => {
+                var contentDispositionHeader = response.headers.get('Content-Disposition');
+                var filename = contentDispositionHeader.match(/(?<=")(?:\\.|[^"\\])*(?=")/)[0];
+                return Promise.all([response.blob(), filename]);
+            })
+            .then((values) => {
+                const blob = values[0];
+                const filename = values[1];
+                const url = URL.createObjectURL(blob);
+                const a = document.createElement('a');
+                a.href = url;
+                a.download = filename;
+                a.click();
+            })
+    }
+
     componentDidMount() {
         fetch(`${configuration.api.baseUrl}/auditlog`, {
             method: 'GET',
@@ -19,38 +42,42 @@ class AuditLogList extends Component {
 
     render() {
         return (
-            <div>
-                <h1>Audit Log</h1>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Date/Time</th>
-                            <th>IP</th>
-                            <th>Action</th>
-                            <th>User</th>
-                            <th>Email</th>
-                            <th>Role</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            this.state.auditLog.map((entry, index) => {
-                                return (
-                                    <tr key={index}>
-                                        <td>{entry.insert_ts}</td>
-                                        <td>{entry.client_ip}</td>
-                                        <td>{entry.action}</td>
-                                        <td>{entry.name}</td>
-                                        <td>{entry.email}</td>
-                                        <td>{entry.role}</td>
-                                    </tr>
+            <>
+                <button onClick={this.handleExport}>Export to CSV</button>
+
+                <div>
+                    <h1>Audit Log</h1>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Date/Time</th>
+                                <th>IP</th>
+                                <th>Action</th>
+                                <th>User</th>
+                                <th>Email</th>
+                                <th>Role</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.auditLog.map((entry, index) => {
+                                    return (
+                                        <tr key={index}>
+                                            <td>{entry.insert_ts}</td>
+                                            <td>{entry.client_ip}</td>
+                                            <td>{entry.action}</td>
+                                            <td>{entry.name}</td>
+                                            <td>{entry.email}</td>
+                                            <td>{entry.role}</td>
+                                        </tr>
+                                    )
+                                }
                                 )
                             }
-                            )
-                        }
-                    </tbody>
-                </table>
-            </div>
+                        </tbody>
+                    </table>
+                </div>
+            </>
         )
     }
 }
