a5ec0980b74c73da661a95c058b6a6456b859d4d
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-08-23 22:51:56 +0100

    Redirect to home when access token expires

--- src/services/api.js
+++ src/services/api.js
@@ -10,7 +10,18 @@ function secureApiFetch(input, init) {
     } else {
         initWithAuth.headers = headers;
     }
-    return fetch(configuration.api.baseUrl + input, init);
+
+    return fetch(configuration.api.baseUrl + input, init)
+        .then((response) => {
+            if (response.status === 401) {
+                window.localStorage.removeItem('accessToken');
+                window.localStorage.removeItem('isAuth');
+                window.localStorage.removeItem('user.id');
+                window.location = '/';
+            }
+
+            return response;
+        });
 }
 
 export default secureApiFetch
