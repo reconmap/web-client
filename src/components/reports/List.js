591ba210b6717f82807ba10bfdeca73d2b3748a3
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-08-24 22:51:35 +0100

    Show saved reports

--- src/components/reports/List.js
+++ src/components/reports/List.js
@@ -3,26 +3,42 @@ import useSetTitle from '../../hooks/useSetTitle';
 import NoResults from '../ui/NoResults';
 import Loading from '../ui/Loading';
 import useFetch from '../../hooks/useFetch';
-import CreateButton from '../ui/buttons/Create';
+import { Link } from 'react-router-dom';
 
 const ReportsList = () => {
-    useSetTitle('Reports');
+    useSetTitle('Save eports');
     const [reports] = useFetch('/reports')
 
     return <div>
         <div className='heading'>
-            <h1>Reports</h1>
-            <CreateButton>Create Report</CreateButton>
+            <h1>Saved reports</h1>
         </div>
         {!reports ? <Loading /> : reports.length === 0 ? <NoResults /> :
-            <div className='flex flex-wrap gap-4 my-4'>
-                {reports.map((report) =>
-                    <article className='base base-reactive w-48 h-64 justify-end'>
-                        <p>{report.name}</p>
-                        <h2>{report.description}</h2>
-                    </article>
-                )}
-            </div>}
+            <table className='w-full my-4'>
+                <thead>
+                    <tr>
+                        <th>Project</th>
+                        <th>Format</th>
+                        <th>Date/Time</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {reports.map((report, index) => {
+                        return (
+                            <tr key={index}>
+                                <td><Link to={`/project/${report.project_id}`}>{report.project_name}</Link></td>
+                                <td>{report.format}</td>
+                                <td>{report.insert_ts}</td>
+                                <td>
+                                    <Link to={`/reports/${report.id}/download`}>Download</Link>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        }
     </div>
 }
 
