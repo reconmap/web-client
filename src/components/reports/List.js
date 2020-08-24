6a322992065d2e8f38e2634ac37abdbc19e8fba8
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-08-24 23:07:35 +0100

    Add links to the download report api

--- src/components/reports/List.js
+++ src/components/reports/List.js
@@ -4,11 +4,28 @@ import NoResults from '../ui/NoResults';
 import Loading from '../ui/Loading';
 import useFetch from '../../hooks/useFetch';
 import { Link } from 'react-router-dom';
+import secureApiFetch from '../../services/api';
 
 const ReportsList = () => {
     useSetTitle('Save eports');
     const [reports] = useFetch('/reports')
-
+    const handleDownload = (reportId) => {
+        secureApiFetch(`/reports/${reportId}/download`, { method: 'GET' })
+            .then(response => {
+                var contentDispositionHeader = response.headers.get('Content-Disposition');
+                var filename = contentDispositionHeader.split('filename=')[1].split(';')[0];
+                return Promise.all([response.blob(), filename]);
+            })
+            .then((values) => {
+                const blob = values[0];
+                const filename = values[1];
+                const url = URL.createObjectURL(blob);
+                const a = document.createElement('a');
+                a.href = url;
+                a.download = filename;
+                a.click();
+            })
+    }
     return <div>
         <div className='heading'>
             <h1>Saved reports</h1>
@@ -31,7 +48,7 @@ const ReportsList = () => {
                                 <td>{report.format}</td>
                                 <td>{report.insert_ts}</td>
                                 <td>
-                                    <Link to={`/reports/${report.id}/download`}>Download</Link>
+                                    <Link to="#" onClick={() => handleDownload(report.id)}>Download</Link>
                                 </td>
                             </tr>
                         )
