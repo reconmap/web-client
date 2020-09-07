96a5c624fc082b5e8edac955d1227cacaca78dcd
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-09-07 22:13:37 +0100

    Pull version from package.json file

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Footer() {
   return <footer className="text-gray-500 font-medium text-center md:text-left py-12 md:py-5">
-      Version <strong className="text-white">0.0.1</strong>
-    </footer>
+    Version <strong className="text-white">{process.env.REACT_APP_VERSION}</strong>
+  </footer>
 }
