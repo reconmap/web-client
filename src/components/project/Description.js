b0f59d69135fd9918611af087324323c9fa73ac4
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-09-07 22:48:06 +0100

    Convert date tag to time

--- src/components/project/Description.js
+++ src/components/project/Description.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ProjectDescription = ({ project }) => {
     return <div className='flex items-start justify-between'>
         <p>{project.description}</p>
-        <p className='text-right'>Created on <date>{project.insert_ts}</date></p>
+        <p className='text-right'>Created on <time datetime={project.insert_ts}>{project.insert_ts}</time></p>
     </div>
 }
 
