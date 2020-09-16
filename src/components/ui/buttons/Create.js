271b89ae2cd6c732f613d93d7d27b02cf031cd3c
Pablo Lizardo <lizardo.pablo@gmail.com>
2020-09-16 17:16:43 -0300

    btn create in users list

--- src/components/ui/buttons/Create.js
+++ src/components/ui/buttons/Create.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { IconPlus } from "../../icons";
+import BtnSecondary from "./BtnSecondary";
 
-const CreateButton = ({ onClick, children }) => <button onClick={onClick} >
-    <IconPlus styling='mr-2' />
-    {children || "Create"}
-</button>
+const CreateButton = ({ onClick, children , fontSize}) => 
+        <BtnSecondary onClick={onClick} fontSize={fontSize} >
+            <IconPlus styling='mr-2' />
+            {children || "Create"}
+        </BtnSecondary>
 
 export default CreateButton
