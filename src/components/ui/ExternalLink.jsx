615fe5d4de6099fa0f30c4ce0225dd42ee6966d5
Pablo Lizardo <lizardo.pablo@gmail.com>
2020-09-28 19:22:10 -0300

    external link stlying

--- src/components/ui/ExternalLink.jsx
+++ src/components/ui/ExternalLink.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { IconExternalLink } from "../icons";
 
 const ExternalLink
     = (props) => {
     return <a target="_blank"
-              rel="noopener noreferrer" {...props}>{props.children}</a>
+            className='border-b-2 border-red-500 text-red-500 hover:text-red-400 hover:border-red-400 inline-flex items-center'
+              rel="noopener noreferrer" {...props}>
+              {props.children}
+              <IconExternalLink size={4} styling="ml-2"/>
+              </a>
 }
 
 export default ExternalLink
