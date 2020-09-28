4bcba4d54b53fe38a7f2017decdd7aa8ffb981ea
Pablo Lizardo <lizardo.pablo@gmail.com>
2020-09-28 19:45:43 -0300

    added styled components, 1st component with embebed style

--- src/components/ui/ExternalLink.jsx
+++ src/components/ui/ExternalLink.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { IconExternalLink } from "../icons";
+import styled from 'styled-components';
+
+const ExternalLinkWrapper = styled.span`
+    display: inline-flex;
+    align-items:center;
+    justify-content:flex-start;
+    color: tomato;
+    border-bottom: 2px solid tomato;
+    &:hover {
+        color: salmon;
+        border-bottom: 2px solid salmon;
+    }
+`;
+
 
 const ExternalLink
     = (props) => {
-    return <a target="_blank"
-            className='border-b-2 border-red-500 text-red-500 hover:text-red-400 hover:border-red-400 inline-flex items-center'
+    return <ExternalLinkWrapper><a target="_blank"
               rel="noopener noreferrer" {...props}>
-              {props.children}
+              {props.children}</a>
               <IconExternalLink size={4} styling="ml-2"/>
-              </a>
+            </ExternalLinkWrapper>
 }
 
 export default ExternalLink
