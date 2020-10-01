4c0b51587ee4f1b42116b44899f4d22a885d9995
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-10-01 19:20:46 +0100

    Do not link to empty urls

--- src/components/ui/ExternalLink.jsx
+++ src/components/ui/ExternalLink.jsx
@@ -5,6 +5,10 @@ import "./ExternalLink.scss"
 
 const ExternalLink
     = (props) => {
+    if (!props.children) {
+        return "-"
+    }
+
     return <span className="ExternalLinkComponent">
         <a target="_blank" rel="noopener noreferrer" {...props}>{props.children}</a>
         <IconExternalLink size={4} styling="ml-2"/>
