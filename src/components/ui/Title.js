bfc6d8d12884949782e26e97b7f620b4c0c52a23
Pablo Lizardo <pablolizardo@mbp.local>
2020-10-03 18:45:19 -0300

    responsive and hovered badges, and fixed table extended to others views

--- src/components/ui/Title.js
+++ src/components/ui/Title.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Title({type, title}) {
     return (
-        <div className='mt-3 mb-2'>
+        <div className='mt-3 mb-4'>
              {type && <p className='text-sm text-red-500 font-medium tracking-wide uppercase mb-0 '>{type}</p>}
              <h3 className=' text-3xl leading-8 font-extrabold tracking-tight  sm:text-4xl sm:leading-10'>{title}</h3>
         </div>
