7115803fe368dfd6b373278f3c3c239750d2a24f
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-10-04 13:32:00 +0100

    Reflect page number in vulnerabilities list url

--- src/components/vulnerabilities/List.js
+++ src/components/vulnerabilities/List.js
@@ -9,52 +9,58 @@ import secureApiFetch from '../../services/api';
 import VulnerabilitiesTable from '../tables/VulnerabilitiesTable';
 import CreateButton from '../ui/buttons/Create';
 
-const VulnerabilitiesList = ({history}) => {
-    useSetTitle('Vulnerabilities')
+const VulnerabilitiesList = ({match, history}) => {
+    let {pageNumber} = match.params;
+    pageNumber = pageNumber !== undefined ? parseInt(pageNumber) : 1;
+    const page = pageNumber - 1;
+
+    useSetTitle(`Vulnerabilities - Page ${pageNumber}`)
 
     const [vulnerabilities, setVulnerabilities] = useState([]);
-    const [pagination, setPagination] = useState({page: 0, total: 0})
+    const [numberPages, setNumberPages] = useState(0)
 
     const handlePrev = () => {
-        setPagination({...pagination, page: pagination.page - 1})
+        history.push(`/vulnerabilities/${pageNumber - 1}`);
     }
     const handleNext = () => {
-        setPagination({...pagination, page: pagination.page + 1})
+        history.push(`/vulnerabilities/${pageNumber + 1}`);
     }
 
     const reloadData = useCallback(() => {
-        secureApiFetch(`/vulnerabilities?page=${pagination.page}`, {method: 'GET'})
+        secureApiFetch(`/vulnerabilities?page=${page}`, {method: 'GET'})
             .then((response) => {
                 if (response.headers.has('X-Page-Count')) {
-                    // noinspection JSCheckFunctionSignatures
-                    setPagination(pagination => {
-                        return {...pagination, total: response.headers.get('X-Page-Count')}
-                    })
+                    setNumberPages(response.headers.get('X-Page-Count'))
                 }
                 return response.json()
             })
             .then((data) => {
                 setVulnerabilities(data);
             })
-    }, [pagination.page]);
+    }, [page]);
 
     useEffect(() => {
         reloadData()
     }, [reloadData])
 
     const destroy = useDelete('/vulnerabilities/', reloadData);
-    const handleCreateVulnerability = () => {history.push(`/vulnerabilities/create`)}
-    return (<>
-        <div className='heading'>
-            <Breadcrumb history={history}/>
-            <Pagination page={pagination.page} total={pagination.total} handlePrev={handlePrev}
-                        handleNext={handleNext}/>
-            <CreateButton onClick={handleCreateVulnerability}>Create Vulnerability</CreateButton>
-        </div>
-        {!vulnerabilities ? <Loading/> : vulnerabilities.length === 0 ? <NoResults/> :
-            <VulnerabilitiesTable vulnerabilities={vulnerabilities} destroy={destroy}/>
-        }
-    </>)
+    const handleCreateVulnerability = () => {
+        history.push(`/vulnerabilities/create`)
+    }
+
+    return (
+        <>
+            <div className='heading'>
+                <Breadcrumb history={history}/>
+                <Pagination page={page} total={numberPages} handlePrev={handlePrev}
+                            handleNext={handleNext}/>
+                <CreateButton onClick={handleCreateVulnerability}>Create Vulnerability</CreateButton>
+            </div>
+            {!vulnerabilities ? <Loading/> : vulnerabilities.length === 0 ? <NoResults/> :
+                <VulnerabilitiesTable vulnerabilities={vulnerabilities} destroy={destroy}/>
+            }
+        </>
+    )
 }
 
 export default VulnerabilitiesList
