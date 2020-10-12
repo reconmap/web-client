280bd4dd612bf3fd3d86a1ab964004458a5825b9
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-10-12 18:24:57 +0100

    Allow to login after pressing Enter

--- src/components/users/Login.js
+++ src/components/users/Login.js
@@ -24,11 +24,6 @@ const Login = (props) => {
     const handlePassword = e => {
         setCredentials({...credentials, password: e.target.value})
     }
-    const handleKeyPress = (e) => {
-        if (e.key === 'Enter') {
-//      handleLogin();
-        }
-    }
 
     const onOk = () => {
         setLoading(false);
@@ -53,25 +48,26 @@ const Login = (props) => {
                         <div className='flex justify-between items-center  w-full flex-col md:flex-row'>
 
                             <section className="flex flex-col w-full sm:w-1/2 max-w-md gap-2 mx-auto text-center">
-                                <h1 className="text-5xl font-bold mb-10">Login</h1>
-                                <label htmlFor="inputUsername" className="sr-only">
-                                    Username
-                                </label>
-                                <input type="text" id="inputUsername" onChange={handleUsername} placeholder="Username"
-                                       required autoFocus/>
-                                <label htmlFor="inputPassword" className="sr-only">
-                                    Password
-                                </label>
-                                <input onKeyPress={handleKeyPress} type="password" id="inputPassword"
-                                       onChange={handlePassword} placeholder="Password" required/>
-                                <BtnPrimary
-                                    onClick={() => login(credentials, onOk, onKo)}
-                                    disabled={!credentials.username || !credentials.password}>
-                                    {!loading ? "Sign in" : "Processing..."}
-                                </BtnPrimary>
-                                {error &&
-                                <p className='flex items-center justify-between border border-red-600 p-3 rounded  text-red-600 text-center'>
-                                    <span className='mx-auto'> Oops... Incorrect username and/or password</span></p>}
+                                <form onSubmit={event => {
+                                    event.preventDefault();
+                                    login(credentials, onOk, onKo);
+                                }}>
+                                    <fieldset>
+                                        <legend className="text-5xl font-bold mb-10">Login</legend>
+                                        <label htmlFor="inputUsername" className="sr-only">Username</label>
+                                        <input type="text" id="inputUsername" onChange={handleUsername}
+                                               placeholder="Username"
+                                               required autoFocus/>
+                                        <label htmlFor="inputPassword" className="sr-only">Password</label>
+                                        <input type="password" id="inputPassword" onChange={handlePassword}
+                                               placeholder="Password" required/>
+                                        <BtnPrimary type="submit">{!loading ? "Sign in" : "Processing..."}</BtnPrimary>
+                                    </fieldset>
+                                    {error &&
+                                    <p className='flex items-center justify-between border border-red-600 p-3 rounded  text-red-600 text-center'>
+                                        <span className='mx-auto'>Incorrect username or password</span>
+                                    </p>}
+                                </form>
                             </section>
                             <i data-feather={'lock'}
                                className=' text-gray-800 order-1 md:order-3 p-5 hidden md:inline'/>
