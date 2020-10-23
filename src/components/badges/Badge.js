1090db772cba1ef6fea4c10f40b7dc39f24021d8
Pablo Lizardo <pablo_lizardo@yahoo.com.ar>
2020-10-23 17:14:47 -0300

    variables from scss to native css vars

--- src/components/badges/Badge.js
+++ src/components/badges/Badge.js
@@ -1,9 +1,16 @@
-
 export default function Badge ({children, color='gray', fontSize='text-sm', icon}) {
     return (
-        <span className={`px-2 py-1 inline-flex items-center leading-5 font-semibold rounded border-2 border-transparent bg-${color}-300 text-${color}-700 ${fontSize}`}>
+        <span style={ styles.badge }>
             {icon}
             {children}
         </span>
     )
+}
+
+const styles = {
+    badge : {
+        backgroundColor: 'var(--bgColor)',
+        padding: 'var(--padding)',
+        border: 'var(--borderWidth) solid transparent',
+    }
 }
