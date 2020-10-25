f55ccfcf74b7c302b6584c740a05fd6649620aee
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-10-25 11:08:36 +0000

    Re-add creation time label or timestamp loses context

--- src/components/ui/Timestamps.js
+++ src/components/ui/Timestamps.js
@@ -1,16 +1,15 @@
-
 const Timestamps = ({insertTs, updateTs}) => {
     const styles = {
-        stamp : {
-            fontSize : `var(--fontSizeXsmall)`,
+        stamp: {
+            fontSize: `var(--fontSizeXsmall)`,
             color: `var(--text-color)`,
             opacity: '.6',
         }
     }
 
     return <>
-        <span style={ styles.stamp }>
-            {/* <strong>Creation time:</strong>&nbsp; */}
+        <span style={styles.stamp}>
+            <strong>Creation time:</strong>&nbsp;
             <time dateTime={insertTs}>{insertTs}</time>
             .
             {updateTs && <> <strong>Modification time:</strong>
