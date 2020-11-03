abf3ca4fefc181ae89ba04843aaa6243fac56d1d
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-11-03 17:22:53 +0000

    Ensure UserRole badge does not fail with unknown role name

--- src/components/badges/UserRoleBadge.js
+++ src/components/badges/UserRoleBadge.js
@@ -1,26 +1,22 @@
-import {IconBookOpen, IconStar} from '../icons';
-
 export default function UserRoleBadge({role}) {
-    const ROLES = {
-        'creator': {color: 'red', icon: <IconStar/>},
-        'writer': {color: 'blue', icon: <IconStar/>},
-        'reader': {color: 'green', icon: <IconBookOpen/>},
+
+    const roles = {
+        'creator': 'red',
+        'writer': 'blue',
+        'reader': 'green',
     }
+
+    const color = roles.hasOwnProperty(role) ? roles[role] : 'yellow';
+
     const styles = {
-        badge : {
-            color: `var(--${ROLES[role].color},white)`,
-            backgroundColor : `var(--${ROLES[role].color}Dark)`,
-            padding: `var(--paddingBox, .3rem .8rem)`,
-            borderRadius: 'var(--borderRadius, 3px)',
-            border: `var(--borderWidth,2px) solid transparent`,
-            fontSize : `var(--fontSizeXsmall)`,
-            fontWeight : 'var(--fontBold)',
-
-        }
+        color: `var(--${color},white)`,
+        backgroundColor: `var(--${color}Dark)`,
+        padding: `var(--paddingBox, .3rem .8rem)`,
+        borderRadius: 'var(--borderRadius, 3px)',
+        border: `var(--borderWidth,2px) solid transparent`,
+        fontSize: `var(--fontSizeXsmall)`,
+        fontWeight: 'var(--fontBold)',
     }
 
-    return <span style={ styles.badge}>
-                {role}
-            </span>
-           
+    return <span style={styles}>{role}</span>
 }
