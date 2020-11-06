a56c9e5dba4c3d269db8e5a4156677f011ce7991
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-11-06 17:11:26 +0000

    Move NoResults component styles to own file

--- src/components/ui/NoResults.js
+++ src/components/ui/NoResults.js
@@ -1,22 +1,9 @@
 import {IconExclamationCircle} from './Icons'
+import './NoResults.css';
 
 export default function NoResults() {
-    const styles = {
-        noResults: {
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'center',
-            width: '100%',
-            height: '100%',
-            fontWeight: 'var(--fontBold)',
-            opacity: '.4',
-            gap: 'var(--padding)',
-            color: 'var(--text-color)'
-        }
-    }
     return (
-        <div style={styles.noResults}>
+        <div className="NoResults">
             <IconExclamationCircle/>
             No results
         </div>
