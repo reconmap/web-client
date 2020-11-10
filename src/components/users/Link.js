f3d5b97edcecca98766dfcafee32ebf5d0554c18
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-11-10 12:06:10 +0000

    Allow userId in UserLink component to be of any type (string, int)

--- src/components/users/Link.js
+++ src/components/users/Link.js
@@ -1,29 +1,17 @@
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 import {IconUser} from '../ui/Icons';
+import './Link.css';
 
 const UserLink = ({userId, children}) => {
-    const styles = {
-        badge: {
-            alignItems: 'center',
-            display: `inline-flex`,
-            borderRadius: 'var(--borderRadius)',
-            fontWeight: 'var(--fontBold)',
-            fontSize: 'var(--fontSizeSmall)',
-            backgroundColor: 'var(--black)',
-            padding: 'calc(var(--padding)/2) var(--margin)',
-        }
-    }
-    return <Link
-        style={styles.badge}
-        to={`/users/${userId}`}>
+    return <Link className="UserLink" to={`/users/${userId}`}>
         <IconUser styling={{color: 'var(--text-color)'}}/>
         {children}
     </Link>
 }
 
 UserLink.propTypes = {
-    userId: PropTypes.number.isRequired
+    userId: PropTypes.any.isRequired
 };
 
-export default UserLink
+export default UserLink;
