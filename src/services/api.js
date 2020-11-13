11306a8254fd26fd8e7047dbac1b6f9ef8b06cb5
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-11-13 00:33:11 +0000

    Redirect to login on network errors (fixes #23)

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,13 @@
 import Configuration from '../Configuration';
 
+function resetSessionStorageAndRedirect() {
+    window.localStorage.removeItem('accessToken');
+    window.localStorage.removeItem('isAuth');
+    window.localStorage.removeItem('user.id');
+    window.localStorage.removeItem('user');
+    window.location = '/';
+}
+
 function secureApiFetch(input, init) {
     const accessToken = localStorage.getItem('accessToken');
     const headers = accessToken !== null ? {Authorization: 'Bearer ' + accessToken} : {};
@@ -13,14 +21,16 @@ function secureApiFetch(input, init) {
     return fetch(Configuration.apiEndpoint + input, init)
         .then((response) => {
             if (response.status === 401) {
-                window.localStorage.removeItem('accessToken');
-                window.localStorage.removeItem('isAuth');
-                window.localStorage.removeItem('user.id');
-                window.localStorage.removeItem('user');
-                window.location = '/';
+                resetSessionStorageAndRedirect();
             }
 
             return response;
+        })
+        .catch(err => {
+            if (err.message.toLowerCase().indexOf('network') !== -1) {
+                resetSessionStorageAndRedirect();
+            }
+            return Promise.reject(err);
         });
 }
 
