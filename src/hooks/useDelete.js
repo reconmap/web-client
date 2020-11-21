79d73cc35bccd3bf3b356b2895f264a0087cf9ed
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-11-21 19:57:52 +0000

    Return confirmation value from useDelete hook

--- src/hooks/useDelete.js
+++ src/hooks/useDelete.js
@@ -1,17 +1,22 @@
 import secureApiFetch from '../services/api'
 import {actionCompletedToast} from '../utilities/toast'
 
-export default function useDelete(endpoint, updateCb, confirmationMessage = `Do you really want to delete this item?`, deleteMessage = 'The item has been deleted.') {
+export default function useDelete(endpoint, successCallback = null, confirmationMessage = `Do you really want to delete this item?`, deleteMessage = 'The item has been deleted.') {
 
     return async (id) => {
         if (window.confirm(confirmationMessage)) {
             try {
                 await secureApiFetch(`${endpoint}${id}`, {method: 'DELETE'})
-                updateCb()
+                if (successCallback) {
+                    successCallback();
+                }
                 actionCompletedToast(deleteMessage);
+                return true;
             } catch (err) {
                 console.error(err)
             }
+        } else {
+            return false;
         }
     };
 }
