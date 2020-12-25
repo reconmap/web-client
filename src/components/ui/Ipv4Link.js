46857684226d10903531e2984396d3fa9232d68b
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-12-25 23:05:01 +0000

    Remove icon from Ipv4Link component

--- src/components/ui/Ipv4Link.js
+++ src/components/ui/Ipv4Link.js
@@ -1,4 +1,3 @@
-import {IconId} from './Icons';
 import ExternalLink from "./ExternalLink";
 
 const Ipv4Link = (props) => {
@@ -6,9 +5,8 @@ const Ipv4Link = (props) => {
         href={`https://www.infobyip.com/ip-${props.value}.html`}
         className='font-mono border-b border-transparent text-sm inline-flex items-center gap-1 hover:opacity-75'
         title={`View information about IP ${props.value}`}>
-        <IconId/>
         {props.value}
     </ExternalLink>
 }
 
-export default Ipv4Link
+export default Ipv4Link;
