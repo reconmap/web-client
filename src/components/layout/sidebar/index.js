3df36be816d54212521063c2887a5b7db8af0322
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-12-29 14:06:49 +0000

    Make sidebar sublinks visible/invisible on parent click

--- src/components/layout/sidebar/index.js
+++ src/components/layout/sidebar/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Links from './Links'
 import {Link, NavLink, useHistory} from 'react-router-dom'
 import {IconBookOpen, IconDashboard} from '../../ui/Icons';
@@ -7,12 +7,24 @@ import './Sidebar.scss';
 
 export default function Sidebar() {
     const history = useHistory();
+
+    const defaultSectionStatuses = Object.assign({}, ...Links.map(link => ({[link.title]: false})));
+    const [sectionStatuses, updateSectionStatuses] = useState(defaultSectionStatuses);
+
     const handleUserManual = () => {
         window.open("https://reconmap.org/user-manual/", '_blank');
     }
+
     const onSupportButtonClick = () => {
         history.push('/support');
     }
+
+    const onParentClick = (ev, link) => {
+        ev.preventDefault();
+
+        updateSectionStatuses({...sectionStatuses, [link.title]: !sectionStatuses[link.title]})
+    }
+
     return (
         <aside className="sidebar">
             <Link to={'/'} data-label='DashBoard'>
@@ -21,17 +33,22 @@ export default function Sidebar() {
             </Link>
             {Links.map((link, index) => {
                 return <React.Fragment key={index}>
-                    <NavLink to={link.to} data-label={link.title} activeClassName='active' exact>
-                        {link.icon}
-                        <span>{link.title}</span>
-                    </NavLink>
-                    {link.sublinks && link.sublinks.map(sublink =>
-                        <NavLink to={sublink.to} data-label={sublink.title} activeClassName='active' className='sublink'
-                                 exact>
-                            {sublink.icon}
-                            <span>{sublink.title}</span>
+                    <div onClick={ev => onParentClick(ev, link)}>
+                        <NavLink to={link.to} data-label={link.title} activeClassName='active' exact>
+                            {link.icon}
+                            <span>{link.title}</span>
                         </NavLink>
-                    )}
+                    </div>
+                    <div style={{display: sectionStatuses[link.title] ? 'block' : 'none'}}>
+                        {link.sublinks && link.sublinks.map(sublink =>
+                            <NavLink to={sublink.to} data-label={sublink.title} activeClassName='active'
+                                     className='sublink'
+                                     exact>
+                                {sublink.icon}
+                                <span>{sublink.title}</span>
+                            </NavLink>
+                        )}
+                    </div>
                 </React.Fragment>
 
             })}
