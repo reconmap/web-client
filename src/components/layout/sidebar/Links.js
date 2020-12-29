09a291b747c38c2f71fed538b401affa0ddbe300
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-12-29 14:40:03 +0000

    Add more sublinks

--- src/components/layout/sidebar/Links.js
+++ src/components/layout/sidebar/Links.js
@@ -5,7 +5,6 @@ import {
     IconDocumentDuplicate,
     IconDownload,
     IconExtensions,
-    IconList,
     IconEye,
     IconFlag,
     IconFolder,
@@ -16,43 +15,70 @@ import {
 } from "../../ui/Icons";
 
 const Links = [
-    {title: 'Vulnerabilities', icon: <IconFlag size={5}/>, to: '/vulnerabilities'},
+    {
+        title: 'Vulnerabilities', icon: <IconFlag size={5}/>, to: '/vulnerabilities', sublinks: [
+            {
+                title: 'Create',
+                icon: <IconPlus size={5}/>,
+                to: '/vulnerabilities/create',
+            }
+        ]
+    },
     {
         title: 'Tasks',
-        icon: <IconClipboardList size={5}/>, 
+        icon: <IconClipboardList size={5}/>,
         to: '/tasks',
         sublinks: [
             {
-                title: 'List',
-                icon: <IconList size={5}/>, 
-                to: '/tasks',
-            },
-            {
                 title: 'Create',
-                icon: <IconPlus size={5}/>, 
+                icon: <IconPlus size={5}/>,
                 to: '/tasks/create',
             }
         ]
-        },
-    {title: 'Projects', icon: <IconFolder size={5}/>, to: '/projects'},
+    },
+    {
+        title: 'Projects', icon: <IconFolder size={5}/>, to: '/projects', sublinks: [
+            {
+                title: 'Create',
+                icon: <IconPlus size={5}/>,
+                to: '/projects/create',
+            }
+        ]
+    },
     {title: 'Templates', icon: <IconDocumentDuplicate size={5}/>, to: '/templates'},
     {title: 'Saved Reports', icon: <IconReport size={5}/>, to: '/reports'},
-    {title: 'Clients', icon: <IconBriefcase size={5}/>, to: '/clients'},
-    {title: 'Audit log', icon: <IconEye size={5}/>, to: '/auditlog'},
-    {title: 'Users and Permissions', icon: <IconUserGroup size={5}/>, to: '/users'},
     {
-        title: 'System Data', 
-        icon: <IconDatabase size={5}/>, 
+        title: 'Clients', icon: <IconBriefcase size={5}/>, to: '/clients', sublinks: [
+            {
+                title: 'Create',
+                icon: <IconPlus size={5}/>,
+                to: '/clients/create',
+            }
+        ]
+    },
+    {title: 'Audit log', icon: <IconEye size={5}/>, to: '/auditlog'},
+    {
+        title: 'Users and Permissions', icon: <IconUserGroup size={5}/>, to: '/users', sublinks: [
+            {
+                title: 'Create',
+                icon: <IconPlus size={5}/>,
+                to: '/users/create',
+            }
+        ]
+    },
+    {
+        title: 'System Data',
+        icon: <IconDatabase size={5}/>,
         to: '/system-data',
         sublinks: [
             {
                 title: 'Import',
-                icon: <IconUpload size={5}/>, 
+                icon: <IconUpload size={5}/>,
                 to: '/system-data/import',
             },
             {
                 title: 'Export',
-                icon: <IconDownload size={5}/>, 
+                icon: <IconDownload size={5}/>,
                 to: '/system-data/export',
             },
         ]
