54f2dbfe84fc54ba16abef4ab7ce4dd5704abef0
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-12-29 14:00:37 +0000

    Extract sidebar styles to own file

--- src/components/layout/sidebar/index.js
+++ src/components/layout/sidebar/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import LINKS from './Links'
+import Links from './Links'
 import {Link, NavLink, useHistory} from 'react-router-dom'
 import {IconBookOpen, IconDashboard} from '../../ui/Icons';
 import SecondaryButton from '../../ui/buttons/Secondary';
+import './Sidebar.scss';
 
 export default function Sidebar() {
     const history = useHistory();
@@ -13,25 +14,26 @@ export default function Sidebar() {
         history.push('/support');
     }
     return (
-        <aside>
+        <aside className="sidebar">
             <Link to={'/'} data-label='DashBoard'>
                 <IconDashboard size={5}/>
                 <span>Dashboard</span>
             </Link>
-            {LINKS.map((link, index) => {
+            {Links.map((link, index) => {
                 return <React.Fragment key={index}>
                     <NavLink to={link.to} data-label={link.title} activeClassName='active' exact>
                         {link.icon}
                         <span>{link.title}</span>
                     </NavLink>
-                    {link.sublinks && link.sublinks.map( sublink => 
-                        <NavLink to={sublink.to} data-label={sublink.title} activeClassName='active' className='sublink' exact>
+                    {link.sublinks && link.sublinks.map(sublink =>
+                        <NavLink to={sublink.to} data-label={sublink.title} activeClassName='active' className='sublink'
+                                 exact>
                             {sublink.icon}
                             <span>{sublink.title}</span>
                         </NavLink>
                     )}
                 </React.Fragment>
-                
+
             })}
             <div id='bottom'>
                 <SecondaryButton onClick={handleUserManual}><IconBookOpen/>User manual</SecondaryButton>
