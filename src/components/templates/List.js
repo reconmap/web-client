12f6162f261dc6ec0b3d109c53813c31e4ca0b81
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2020-12-31 13:28:45 +0000

    Fix link to import templates

--- src/components/templates/List.js
+++ src/components/templates/List.js
@@ -35,7 +35,7 @@ const TemplatesList = ({history}) => {
         <>
             <div className='heading'>
                 <Breadcrumb/>
-                <CreateButton onClick={() => history.push('/system-data')}>Import template(s)</CreateButton>
+                <CreateButton onClick={() => history.push('/system-data/import')}>Import template(s)</CreateButton>
             </div>
             <Title title='Templates' icon={<IconDocumentDuplicate/>}/>
             {!templates ? <Loading/> :
