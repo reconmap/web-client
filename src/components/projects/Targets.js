6647d8060b9f8546aa4bc2d2d361477566823c8c
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2021-01-19 22:17:41 +0000

    Add breadcrumbs to target page

--- src/components/projects/Targets.js
+++ src/components/projects/Targets.js
@@ -1,6 +1,6 @@
 import Loading from "../ui/Loading"
-import {IconPlus, IconServer} from '../ui/Icons'
-import {useHistory} from 'react-router-dom'
+import { IconPlus, IconServer } from '../ui/Icons'
+import { useHistory } from 'react-router-dom'
 import SecondaryButton from '../ui/buttons/Secondary'
 import useFetch from "../../hooks/useFetch";
 import React from "react";
@@ -8,7 +8,7 @@ import DeleteButton from "../ui/buttons/Delete";
 import secureApiFetch from "../../services/api";
 import NoResultsTableRow from "../ui/NoResultsTableRow";
 
-const ProjectTargets = ({project}) => {
+const ProjectTargets = ({ project }) => {
     const history = useHistory()
 
     const [targets, updateTargets] = useFetch(`/targets?projectId=${project.id}`)
@@ -20,7 +20,7 @@ const ProjectTargets = ({project}) => {
     const onDeleteButtonClick = (ev, targetId) => {
         ev.preventDefault();
 
-        secureApiFetch(`/targets/${targetId}`, {method: 'DELETE'})
+        secureApiFetch(`/targets/${targetId}`, { method: 'DELETE' })
             .then(() => {
                 updateTargets();
             })
@@ -28,37 +28,37 @@ const ProjectTargets = ({project}) => {
 
     return <section>
         <h4>
-            <IconServer/>Targets
-            <SecondaryButton onClick={handleAddTarget}><IconPlus/>Add target</SecondaryButton>
+            <IconServer />Targets
+            <SecondaryButton onClick={handleAddTarget}><IconPlus />Add target</SecondaryButton>
         </h4>
-        {!targets ? <Loading/> :
+        {!targets ? <Loading /> :
             <table>
                 <thead>
-                <tr>
-                    <th>Kind</th>
-                    <th>Name</th>
-                    <th>Vulnerable?</th>
-                    <th>&nbsp;</th>
-                </tr>
+                    <tr>
+                        <th>Name</th>
+                        <th>Kind</th>
+                        <th>Vulnerable?</th>
+                        <th>&nbsp;</th>
+                    </tr>
                 </thead>
                 <tbody>
-                {targets.length === 0 && <NoResultsTableRow numColumns={4}/>}
-                {targets.map((target, index) =>
-                    <tr key={index}>
-                        <td><IconServer/> {target.kind}</td>
-                        <td><a
-                            href="/"
-                            onClick={(ev) => {
-                                ev.preventDefault();
-                                history.push(`/projects/${project.id}/targets/${target.id}`)
-                            }}>{target.name}</a>
-                        </td>
-                        <td>{target.num_vulnerabilities > 0 ? `Yes (${target.num_vulnerabilities} vulnerabilities found)` : "No"}</td>
-                        <td>
-                            <DeleteButton onClick={ev => onDeleteButtonClick(ev, target.id)}/>
-                        </td>
-                    </tr>
-                )}
+                    {targets.length === 0 && <NoResultsTableRow numColumns={4} />}
+                    {targets.map((target, index) =>
+                        <tr key={index}>
+                            <td><a
+                                href="/"
+                                onClick={(ev) => {
+                                    ev.preventDefault();
+                                    history.push(`/projects/${project.id}/targets/${target.id}`)
+                                }}>{target.name}</a>
+                            </td>
+                            <td><IconServer /> {target.kind}</td>
+                            <td>{target.num_vulnerabilities > 0 ? `Yes (${target.num_vulnerabilities} vulnerabilities found)` : "No"}</td>
+                            <td>
+                                <DeleteButton onClick={ev => onDeleteButtonClick(ev, target.id)} />
+                            </td>
+                        </tr>
+                    )}
                 </tbody>
             </table>
         }
--- src/components/target/Create.js
+++ src/components/target/Create.js
@@ -1,37 +1,45 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import secureApiFetch from '../../services/api';
 import Breadcrumb from '../ui/Breadcrumb';
 import TargetKinds from '../../models/TargetKinds'
 import PrimaryButton from '../ui/buttons/Primary';
 import Title from '../ui/Title';
+import { Link } from 'react-router-dom';
+import useFetch from '../../hooks/useFetch';
 
-export default function TargetCreateForm({match, history}) {
+export default function TargetCreateForm({ match, history }) {
     const projectId = match.params.id;
-    const [newTarget, setNewTarget] = useState({projectId: projectId, name: null, kind: TargetKinds[0].value})
+    const [newTarget, setNewTarget] = useState({ projectId: projectId, name: null, kind: TargetKinds[0].value })
     const [loading, setLoading] = useState(false)
+
+    const [project] = useFetch(`/projects/${projectId}`)
+
     const handleCreate = async (ev) => {
         ev.preventDefault();
 
         setLoading(true)
-        await secureApiFetch(`/targets`, {method: 'POST', body: JSON.stringify(newTarget)})
+        await secureApiFetch(`/targets`, { method: 'POST', body: JSON.stringify(newTarget) })
         history.push(`/projects/${projectId}`)
     }
     const handleFormChange = ev => {
         const target = ev.target;
         const name = target.name;
         const value = target.value;
-        setNewTarget({...newTarget, [name]: value});
+        setNewTarget({ ...newTarget, [name]: value });
     };
 
     return (
         <div>
             <div className='heading'>
-                <Breadcrumb/>
+                <Breadcrumb>
+                    <Link to="/projects">Projects</Link>
+                    {project && <Link to={`/projects/${project.id}`}>{project.name}</Link>}
+                </Breadcrumb>
             </div>
             <form onSubmit={handleCreate}>
-                <Title title='Create Target'/>
+                <Title title='Create Target' />
                 <label>Name
-                    <input type="text" name="name" onChange={handleFormChange} required autoFocus/></label>
+                    <input type="text" name="name" onChange={handleFormChange} required autoFocus /></label>
                 <label>Kind
                     <select name="kind" onChange={handleFormChange}>
                         {TargetKinds.map((targetKind, index) =>
@@ -40,7 +48,7 @@ export default function TargetCreateForm({match, history}) {
                     </select>
                 </label>
                 <PrimaryButton type="submit"
-                               disabled={loading}>Create</PrimaryButton>
+                    disabled={loading}>Create</PrimaryButton>
             </form>
         </div>
     )
