ac914604c743dc1b2e935bf64ab37f40c923a586
Pablo Lizardo <pablo_lizardo@yahoo.com.ar>
2021-01-21 19:31:31 -0300

    feat: :sparkles: copy code to clipboard

--- src/components/ui/ShellCommand.js
+++ src/components/ui/ShellCommand.js
@@ -1,7 +1,28 @@
+import { useRef } from 'react';
+import { IconDocumentDuplicate } from './Icons';
 import './ShellCommand.scss';
+import {actionCompletedToast} from '../../components/ui/toast'
 
 const ShellCommand = ({children}) => {
-    return <code>{children}</code>
+    const codeRef = useRef()
+    const handleCopy = () => {
+        const code = codeRef.current.innerText
+        
+        var textField = document.createElement('textarea')
+        textField.innerText = code
+        document.body.appendChild(textField)
+        textField.select()
+        document.execCommand('copy')
+        textField.remove()
+        actionCompletedToast('Copied to clipboard');
+    
+    }
+    return <code ref={codeRef}>
+            {children}
+            <button onClick={handleCopy}>
+                <IconDocumentDuplicate />
+            </button>
+        </code>
 }
 
 export default ShellCommand;
