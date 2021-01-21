c6791013c8b5e948b7b14a4e25b218477730eed6
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2021-01-21 22:31:24 +0000

    Introduce task command tab

---
+++ src/components/tasks/CommandTab.js
@@ -0,0 +1,120 @@
+import ShellCommand from "../ui/ShellCommand";
+import NoResultsTableRow from '../ui/NoResultsTableRow'
+import SecondaryButton from '../ui/buttons/Secondary'
+import FileSizeSpan from '../ui/FileSizeSpan'
+import { IconUpload } from "../ui/Icons";
+import { useEffect, useState } from "react";
+import secureApiFetch from "src/services/api";
+import DeleteButton from "../ui/buttons/Delete";
+import Loading from "../ui/Loading";
+import PrimaryButton from "../ui/buttons/Primary";
+import { actionCompletedToast } from "../ui/toast";
+import useFetch from "src/hooks/useFetch";
+
+const TaskCommandTab = ({ task }) => {
+    const [commandVars, setCommandVars] = useState('');
+    const [containerArgs, setContainerArgs] = useState(null);
+    const [commandOutputs, updateCommandOutputs] = useFetch(`/commands/outputs?taskId=${task.id}`)
+
+    const onDeleteOutputClick = (ev, outputId) => {
+        ev.preventDefault();
+
+        secureApiFetch(`/commands/outputs/${outputId}`, { method: 'DELETE' })
+            .then(() => {
+                actionCompletedToast("The output has been deleted.");
+                updateCommandOutputs();
+            })
+            .catch(err => console.error(err))
+    }
+
+    const onArgUpdate = ev => {
+        setContainerArgs({ ...containerArgs, [ev.target.name]: { name: ev.target.name, placeholder: ev.target.value } });
+    };
+
+    useEffect(() => {
+        const argRegex = /{{{(.+?)}}}/g;
+        const commandArguments = task.command_container_args.match(argRegex);
+        const argMap = commandArguments.reduce((accumulator, current) => {
+            const tokens = current.replaceAll('{{{', '').replaceAll('}}}', '').split('|||');
+            accumulator[tokens[0]] = {
+                name: tokens[0],
+                placeholder: tokens[1]
+            };
+            return accumulator;
+        }, {});
+
+        setContainerArgs(argMap);
+
+        if (containerArgs) {
+            let dynamicArgs = '';
+            Object.keys(containerArgs).forEach((key) => {
+                let containerArg = containerArgs[key];
+                dynamicArgs += `-var ${containerArg.name}=${containerArg.placeholder}`;
+            });
+            setCommandVars(dynamicArgs);
+        }
+    }, [containerArgs, task]);
+
+    return <>
+        <h4>Command</h4>
+        {task.command_id &&
+            <>
+                <h5>1. Fill in the arguments</h5>
+                {containerArgs !== null &&
+                    Object.keys(containerArgs).map((key) =>
+                        <p>
+                            {containerArgs[key].name}<br />
+                            <input name={containerArgs[key].name} value={containerArgs[key].placeholder} onChange={onArgUpdate} />
+                        </p>
+                    )
+                }
+                <h5>2. Run this command</h5>
+                <div>
+                    To run the task execute:
+                                <ShellCommand>./rmap run-command -id {task.command_id} {commandVars}</ShellCommand>
+                    <p style={{ fontSize: "small" }}>(this will invoke the <strong>{task.command_short_name}</strong> application on a docker container)</p>
+                </div>
+                <h4>
+                    <h5>Results</h5>
+                    <PrimaryButton to={`/tasks/${task.id}/upload`}>
+                        <IconUpload /> Upload results
+                                </PrimaryButton>
+                </h4>
+                {!commandOutputs ? <Loading /> :
+
+                    <table>
+                        <thead>
+                            <th>Upload date</th>
+                            <th>Uploaded by</th>
+                            <th>Filename</th>
+                            <th>File size</th>
+                            <th>Mimetype</th>
+                            <th>&nbsp;</th>
+                        </thead>
+                        <tbody>
+                            {commandOutputs.length === 0 && <NoResultsTableRow numColumns={5} />}
+                            {commandOutputs.map((result, index) =>
+                                <tr key={index}>
+                                    <td>{result.insert_ts}</td>
+                                    <td>{result.submitter_name}</td>
+                                    <td>{result.file_name}</td>
+                                    <td><FileSizeSpan fileSize={result.file_size} /></td>
+                                    <td>{result.file_mimetype}</td>
+                                    <td style={{ display: "flex" }}>
+                                        <SecondaryButton disabled>Download</SecondaryButton>
+                                        <DeleteButton onClick={ev => onDeleteOutputClick(ev, result.id)} />
+                                    </td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                }
+            </>
+        }
+        {!task.command_id &&
+            <p>No command defined for this task.</p>
+        }
+    </>
+}
+
+export default TaskCommandTab;
--- src/components/tasks/Details.js
+++ src/components/tasks/Details.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import secureApiFetch from '../../services/api'
 import PrimaryButton from '../ui/buttons/Primary'
-import { IconClipboard, IconUpload } from '../ui/Icons'
+import { IconClipboard } from '../ui/Icons'
 import Title from './../ui/Title'
 import ButtonGroup from "../ui/buttons/ButtonGroup";
 import DeleteButton from "../ui/buttons/Delete";
@@ -12,19 +12,17 @@ import { actionCompletedToast } from "../ui/toast";
 import useFetch from './../../hooks/useFetch'
 import useDelete from '../../hooks/useDelete'
 import { Link } from "react-router-dom";
-import ShellCommand from "../ui/ShellCommand";
 import TextBlock from "../ui/TextBlock";
 import TaskStatusFormatter from "./TaskStatusFormatter";
 import TaskStatuses from "../../models/TaskStatuses";
-import NoResultsTableRow from '../ui/NoResultsTableRow'
-import SecondaryButton from '../ui/buttons/Secondary'
-import FileSizeSpan from '../ui/FileSizeSpan'
+import Tabs from '../ui/Tabs'
+import Tab from '../ui/Tab'
+import TaskCommandTab from './CommandTab'
 
 const TaskDetails = ({ history, match }) => {
     const taskId = match.params.taskId;
     const [task, fetchTask] = useFetch(`/tasks/${taskId}`)
     const [users] = useFetch(`/users`)
-    const [results, updateCommandOutputs] = useFetch(`/commands/outputs?taskId=${taskId}`)
     const [project, setProject] = useState(null);
     const destroy = useDelete('/tasks/', fetchTask);
 
@@ -59,19 +57,6 @@ const TaskDetails = ({ history, match }) => {
             .catch(err => console.error(err))
     }
 
-    const onOutputButtonClick = (ev, outputId) => {
-        ev.preventDefault();
-
-        secureApiFetch(`/commands/outputs/${outputId}`, { method: 'DELETE' })
-            .then(() => {
-                actionCompletedToast("The output has been deleted.");
-                updateCommandOutputs();
-            })
-            .catch(err => console.error(err))
-    }
-
-    const [dynamicArgs, setDynamicArgs] = useState('');
-
     useEffect(() => {
         if (task) {
             document.title = `Task ${task.name} | Reconmap`;
@@ -82,38 +67,8 @@ const TaskDetails = ({ history, match }) => {
                     setProject(project);
                 })
                 .catch(err => console.error(err))
-
-            const argRegex = /{{{(.+?)}}}/g;
-            const commandArguments = task.command_container_args.match(argRegex);
-            const argMap = commandArguments.reduce((accumulator, current) => {
-                const tokens = current.replaceAll('{{{', '').replaceAll('}}}', '').split('|||');
-                accumulator[tokens[0]] = {
-                    name: tokens[0],
-                    placeholder: tokens[1]
-                };
-                return accumulator;
-            }, {});
-
-            setContainerArgs(argMap);
         }
     }, [task])
-
-    const onArgUpdate = ev => {
-        setContainerArgs({ ...containerArgs, [ev.target.name]: { name: ev.target.name, placeholder: ev.target.value } });
-    };
-
-    const [containerArgs, setContainerArgs] = useState(null);
-
-    useEffect(() => {
-        if (containerArgs) {
-            var aa = '';
-            Object.keys(containerArgs).forEach((key) => {
-                let a = containerArgs[key];
-                aa += `-var ${a.name}=${a.placeholder}`;
-            });
-            setDynamicArgs(aa);
-        }
-    }, [containerArgs]);
 
     return (
         <div>
@@ -140,92 +95,40 @@ const TaskDetails = ({ history, match }) => {
                 <article>
                     <Title title={task.name} type='Task' icon={<IconClipboard />} />
                     <Timestamps insertTs={task.insert_ts} updateTs={task.update_ts} />
-                    <div className="two-columns">
-                        <div>
-                            <h4>Description</h4>
-                            <TextBlock value={task.description || "(empty)"} />
-                            <h4>Status</h4>
-                            <p style={{ display: 'flex', alignItems: 'center', columnGap: 'var(--margin)' }}>
-                                <TaskStatusFormatter task={task} />
-                            </p>
-                        </div>
-                        <div>
-                            <h4>People</h4>
-                            <dl>
-                                <dt>Created by</dt>
-                                <dd>{task.creator_full_name}</dd>
-
-                                <dt>Assigned to</dt>
-                                <dd>
-                                    <select onChange={onAssigneeChange} defaultValue={task.assignee_uid}>
-                                        <option value="">(nobody)</option>
-                                        {users && users.map((user, index) =>
-                                            <option key={index} value={user.id}>{user.full_name}</option>
-                                        )}
-                                    </select>
-                                </dd>
-                            </dl>
-                        </div>
-                    </div>
-
-                    <h4>Command</h4>
-                    {task.command_id &&
-                        <>
-                            <h5>1. Fill in the arguments</h5>
-                            {containerArgs !== null &&
-                                Object.keys(containerArgs).map((key) =>
-                                    <p>
-                                        {containerArgs[key].name}<br />
-                                        <input name={containerArgs[key].name} value={containerArgs[key].placeholder} onChange={onArgUpdate} />
+                    <Tabs>
+                        <Tab name="Details">
+                            <div className="two-columns">
+                                <div>
+                                    <h4>Description</h4>
+                                    <TextBlock value={task.description || "(empty)"} />
+                                    <h4>Status</h4>
+                                    <p style={{ display: 'flex', alignItems: 'center', columnGap: 'var(--margin)' }}>
+                                        <TaskStatusFormatter task={task} />
                                     </p>
-                                )
-                            }
-                            <h5>2. Run this command</h5>
-                            <div>
-                                To run the task execute:
-                                <ShellCommand>./rmap run-command -id {task.command_id} {dynamicArgs}</ShellCommand>
-                                <p style={{ fontSize: "small" }}>(this will invoke the <strong>{task.command_short_name}</strong> application on a docker container)</p>
-                            </div>
-                            <h4>
-                                <h5>Results</h5>
-                                <PrimaryButton to={`/tasks/${task.id}/upload`}>
-                                    <IconUpload /> Upload results
-                                </PrimaryButton>
-                            </h4>
-                            {!results ? <Loading /> :
+                                </div>
+                                <div>
+                                    <h4>People</h4>
+                                    <dl>
+                                        <dt>Created by</dt>
+                                        <dd>{task.creator_full_name}</dd>
 
-                                <table>
-                                    <thead>
-                                        <th>Upload date</th>
-                                        <th>Uploaded by</th>
-                                        <th>Filename</th>
-                                        <th>File size</th>
-                                        <th>Mimetype</th>
-                                        <th>&nbsp;</th>
-                                    </thead>
-                                    <tbody>
-                                        {results.length === 0 && <NoResultsTableRow numColumns={5} />}
-                                        {results.map((result, index) =>
-                                            <tr key={index}>
-                                                <td>{result.insert_ts}</td>
-                                                <td>{result.submitter_name}</td>
-                                                <td>{result.file_name}</td>
-                                                <td><FileSizeSpan fileSize={result.file_size} /></td>
-                                                <td>{result.file_mimetype}</td>
-                                                <td style={{ display: "flex" }}>
-                                                    <SecondaryButton disabled>Download</SecondaryButton>
-                                                    <DeleteButton onClick={ev => onOutputButtonClick(ev, result.id)} />
-                                                </td>
-                                            </tr>
-                                        )}
-                                    </tbody>
-                                </table>
-                            }
-                        </>
-                    }
-                    {!task.command_id &&
-                        <p>No command defined for this task.</p>
-                    }
+                                        <dt>Assigned to</dt>
+                                        <dd>
+                                            <select onChange={onAssigneeChange} defaultValue={task.assignee_uid}>
+                                                <option value="">(nobody)</option>
+                                                {users && users.map((user, index) =>
+                                                    <option key={index} value={user.id}>{user.full_name}</option>
+                                                )}
+                                            </select>
+                                        </dd>
+                                    </dl>
+                                </div>
+                            </div>
+                        </Tab>
+                        <Tab name="Command">
+                            <TaskCommandTab task={task} />
+                        </Tab>
+                    </Tabs>
                 </article>
             }
         </div >
