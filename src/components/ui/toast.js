698a3e9a4eea1945e9a554e0a632852433856384
Pablo Lizardo <pablo.lizardo@aerolab.co>
2021-05-30 12:54:28 -0300

    fixed toast color, typo and position

--- src/components/ui/toast.js
+++ src/components/ui/toast.js
@@ -3,5 +3,11 @@ import { createStandaloneToast } from "@chakra-ui/react"
 export const toast = createStandaloneToast()
 
 export function actionCompletedToast(msg) {
-    toast({ title:'Action Completed', description: msg})
+    toast({ 
+        title:'Action completed', 
+        description: msg, 
+        position:"bottom-right",
+        isClosable: true,
+        status:'error'
+    })
 }
