273d5d4d90829f9e1828a5257ab3d44bd6efbe1e
Pablo Lizardo <pablo_lizardo@yahoo.com.ar>
2021-08-15 12:24:01 -0300

    fix: updated delete button in order to center properly the icon (#92)
    Co-authored-by: Pablo Lizardo <pablo.lizardo@aerolab.co>

--- src/components/ui/buttons/DeleteIconButton.js
+++ src/components/ui/buttons/DeleteIconButton.js
@@ -1,6 +1,6 @@
 import { DeleteIcon } from "@chakra-ui/icons";
 import { IconButton } from "@chakra-ui/react";
 
-const DeleteIconButton = (props) => <IconButton colorScheme="red" onClick={props.onClick} {...props} leftIcon={<DeleteIcon />} />
+const DeleteIconButton = (props) => <IconButton colorScheme="red" onClick={props.onClick} {...props} icon={<DeleteIcon />} />
 
 export default DeleteIconButton;
