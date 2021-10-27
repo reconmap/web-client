e240f7b3acd6063f2290303f1eb52c511dc6b620
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2021-10-27 22:22:45 +0100

    Use chakra table for audit log

--- src/components/auditlog/AuditLogsTable.js
+++ src/components/auditlog/AuditLogsTable.js
@@ -1,3 +1,4 @@
+import { Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/table';
 import Badge from 'components/badges/Badge';
 import UserRoleBadge from 'components/badges/UserRoleBadge';
 import Ipv4Link from 'components/ui/Ipv4Link';
@@ -7,48 +8,44 @@ import UserLink from 'components/users/Link';
 
 const AuditLogsTable = ({ auditLog, hideUserColumns = false }) => {
     const numColumns = hideUserColumns ? 4 : 6;
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Action</th>
-                    <th>IP address</th>
-                    <th>User agent</th>
-                    <th>Date/Time</th>
+    return <Table size="sm">
+        <Thead>
+            <Tr>
+                <Th>Action</Th>
+                <Th>IP address</Th>
+                <Th>User agent</Th>
+                <Th>Date/Time</Th>
+                {!hideUserColumns &&
+                    <>
+                        <Th>User</Th>
+                        <Th>Role</Th>
+                    </>
+                }
+                <Th>Object</Th>
+            </Tr>
+        </Thead>
+        <Tbody>
+            {auditLog.length === 0 && <NoResultsTableRow numColumns={numColumns} />}
+            {auditLog.map(entry => {
+                return <Tr key={entry.id}>
+                    <Td>
+                        <Badge>{entry.action}</Badge>
+                    </Td>
+                    <Td><Ipv4Link value={entry.client_ip} /></Td>
+                    <Td>{entry.user_agent ? <UserAgentLabel userAgent={entry.user_agent} /> : '-'}</Td>
+                    <Td>{entry.insert_ts}</Td>
                     {!hideUserColumns &&
                         <>
-                            <th>User</th>
-                            <th>Role</th>
+                            <Td>{entry.user_name ?
+                                <UserLink userId={entry.user_id}>{entry.user_name}</UserLink> : '-'}</Td>
+                            <Td><UserRoleBadge role={entry.user_role} /></Td>
                         </>
                     }
-                    <th>Object</th>
-                </tr>
-            </thead>
-            <tbody>
-                {auditLog.length === 0 && <NoResultsTableRow numColumns={numColumns} />}
-                {auditLog.map((entry, index) => {
-                    return (
-                        <tr key={index}>
-                            <td>
-                                <Badge>{entry.action}</Badge>
-                            </td>
-                            <td><Ipv4Link value={entry.client_ip} /></td>
-                            <td>{entry.user_agent ? <UserAgentLabel userAgent={entry.user_agent} /> : '-'}</td>
-                            <td>{entry.insert_ts}</td>
-                            {!hideUserColumns &&
-                                <>
-                                    <td>{entry.user_name ?
-                                        <UserLink userId={entry.user_id}>{entry.user_name}</UserLink> : '-'}</td>
-                                    <td><UserRoleBadge role={entry.user_role} /></td>
-                                </>
-                            }
-                            <td>{entry.object}</td>
-                        </tr>
-                    )
-                })}
-            </tbody>
-        </table>
-    )
+                    <Td>{entry.object}</Td>
+                </Tr>
+            })}
+        </Tbody>
+    </Table>
 }
 
 export default AuditLogsTable;
