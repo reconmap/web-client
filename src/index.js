bbe9177820ba6fdae253e6f46a2ea28ab023d049
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2021-12-05 23:32:58 +0000

    Upgrade dependencies

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import TimeAgo from 'javascript-time-ago';
 import en from 'javascript-time-ago/locale/en';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import theme from 'styles/theme';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -13,11 +12,9 @@ TimeAgo.addDefaultLocale(en);
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <ChakraProvider theme={theme}>
-                <App />
-            </ChakraProvider>
-        </BrowserRouter>
+        <ChakraProvider theme={theme}>
+            <App />
+        </ChakraProvider>
     </React.StrictMode >, document.getElementById('root'));
 
 serviceWorker.unregister();
