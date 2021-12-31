a7ab3780c3c25385614faf89430ccfca863a2f30
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2021-12-31 11:28:57 +0000

    Save/retrieve user preferences from server

--- src/services/userPreferences.js
+++ src/services/userPreferences.js
@@ -6,16 +6,12 @@ const DefaultUserPreferences = {
 
 const initialiseUserPreferences = user => {
     if (!user.hasOwnProperty('preferences') || null === user.preferences || undefined === user.preferences) {
-        console.debug("user has no preferences")
         return DefaultUserPreferences;
     }
     if (typeof (user.preferences) === 'string') {
-        console.debug("user prefs are string")
         return { ...DefaultUserPreferences, ...JSON.parse(user.preferences) };
     }
 
-    console.debug("user prefs are object")
-    console.dir(user.preferences);
     return { ...DefaultUserPreferences, ...user.preferences };
 }
 
