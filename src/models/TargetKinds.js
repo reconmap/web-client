e73a96920bde1ee4c38ba9c00142a2108fd69866
cccaaasser <BossmanK2A@gmail.com>
2022-02-08 11:16:53 +0700

    Update TargetKinds.js
    Add mobile application

--- src/models/TargetKinds.js
+++ src/models/TargetKinds.js
@@ -4,6 +4,7 @@ const TargetKinds = [
     {value: 'cidr_range', description: 'CIDR range (e.g. 192.168.100.0/24, 2001:DB8::/48)'},
     {value: 'url', description: 'URL (e.g. https://webapp.net, ftp://198.127.2.3)'},
     {value: 'binary', description: 'Binary (e.g. protectedapp.exe, apiserver.jar)'},
+    {value: 'Mobile Application', description: 'Mobile application (e.g. protectedapp.apk, securebank.ipa)'}
 ];
 
 export default TargetKinds
