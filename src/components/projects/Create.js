aff5a3505dd201642566f96b9fd901b79c602547
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2022-04-19 21:56:22 +0100

    Check response status code after creating project

--- src/components/projects/Create.js
+++ src/components/projects/Create.js
@@ -1,5 +1,6 @@
-import { actionCompletedToast } from 'components/ui/toast';
+import { actionCompletedToast, errorToast } from 'components/ui/toast';
 import useQuery from 'hooks/useQuery';
+import { StatusCodes } from 'http-status-codes';
 import Project from 'models/Project';
 import { useState } from 'react';
 import { Link, useNavigate } from "react-router-dom";
@@ -18,29 +19,33 @@ const ProjectCreate = () => {
     const handleCreate = async (ev) => {
         ev.preventDefault();
 
-        await secureApiFetch(`/projects`, { method: 'POST', body: JSON.stringify(newProject) })
+        secureApiFetch(`/projects`, { method: 'POST', body: JSON.stringify(newProject) })
+            .then(resp => {
+                if (resp.status !== StatusCodes.CREATED) {
+                    errorToast("There was a problem creating the project. Please check the form and try again");
+                    return;
+                }
 
-        if (newProject.is_template) {
-            navigate('/projects/templates');
-        } else {
-            navigate('/projects');
-        }
+                actionCompletedToast(`The project '${newProject.name}' has been created`);
 
-        actionCompletedToast(`The project '${newProject.name}' has been created`);
+                if (newProject.is_template) {
+                    navigate('/projects/templates');
+                } else {
+                    navigate('/projects');
+                }
+            })
     }
 
-    return (
-        <div>
-            <div className="heading">
-                <Breadcrumb>
-                    <Link to="/projects">Projects</Link>
-                </Breadcrumb>
-            </div>
-            <Title title="New project details" icon={<IconPlus />} />
-
-            <ProjectForm project={newProject} projectSetter={setNewProject} onFormSubmit={handleCreate} />
+    return <div>
+        <div className="heading">
+            <Breadcrumb>
+                <Link to="/projects">Projects</Link>
+            </Breadcrumb>
         </div>
-    )
+        <Title title="New project details" icon={<IconPlus />} />
+
+        <ProjectForm project={newProject} projectSetter={setNewProject} onFormSubmit={handleCreate} />
+    </div>
 }
 
 export default ProjectCreate
