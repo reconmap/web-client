4561fd57cd6b8c3fab5f1c40106686e5f89f1677
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2025-12-11 21:59:01 +0000

    Tidy up

--- packages/app/src/components/badges/UserAvatar.jsx
+++ packages/app/src/components/badges/UserAvatar.jsx
@@ -1,7 +1,7 @@
 import CssIcon from "components/ui/CssIcon";
 
-const UserAvatar = ({ email, size = "sm", onClick }) => {
-    return <CssIcon name="user" />;
+const UserAvatar = ({ }) => {
+    return <><CssIcon name="user" /></>;
 };
 
 export default UserAvatar;
