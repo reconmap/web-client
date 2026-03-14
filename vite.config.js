7540bb799f919a5dfb9e2035e774c0bfbc4d3a76
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2026-03-14 22:48:46 +0000

    Upgrade to vite8 and remove viteTsconfigPaths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgrPlugin from "vite-plugin-svgr";
-import viteTsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 /** @type {import('vite').UserConfig} */
@@ -13,8 +12,9 @@ export default defineConfig({
     build: {
         outDir: "build",
     },
-    plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+    plugins: [react(), svgrPlugin()],
     resolve: {
+        tsconfigPaths: true,
         alias: {
         },
     },
